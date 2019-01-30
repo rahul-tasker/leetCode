cae0fd060e175ae38ba8445088749fd6757c0760
Rahul Tasker <rahultasker@rahuls-air.bc.edu>
2019-01-29 19:07:32 -0500

    commit SingleNumber2

---
+++ SingleNumber2.java
@@ -0,0 +1,34 @@
+/*
+Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+
+Example 1:
+
+Input: [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: [0,1,0,1,0,1,99]
+Output: 99
+
+Runtime: 6 ms, faster than 25.71% of Java online submissions
+*/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i ++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for(Integer key : map.keySet()) {
+            if(map.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
+    }
+}
